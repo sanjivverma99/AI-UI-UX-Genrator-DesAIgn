ad694db92648cf619b9b94dcc89546583697002a
sanjivverma99 <sanjivverm@gmail.com>
2026-01-13 01:05:54 +0530

    Initial Commit (Project Setup)

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const appFont = DM_Sans({
+  subsets:['latin']
+})
+
+export const metadata: Metadata = {
+  title: "UIUX Mockup Generator App",
+  description: "Generate High Quality Free UIUX Mobile and Web mokup Designs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={appFont.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>
+      Sanjiv
+      </h2>
+      <Button>Get Start</Button>
+    </div>
+  );
+}
